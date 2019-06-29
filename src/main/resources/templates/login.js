1e29f7874900b56b1c8db0dff743272114814b5a
2017302580221 <43109812+2017302580221@users.noreply.github.com>
2019-06-29 20:25:39 +0800

    6.29.2

---
+++ src/main/resources/templates/login.js
@@ -0,0 +1,23 @@
+function login() {
+
+    var username = document.getElementById("email");
+    var pass = document.getElementById("password");
+
+    if (username.value == "") {
+
+        alert("请输入用户名");
+
+    } else if (pass.value  == "") {
+
+        alert("请输入密码");
+
+    } else if(username.value == "zzw@250" && pass.value == "123456"){
+
+        window.location.href="view.html";
+
+    } else {
+
+        alert("请输入正确的用户名和密码！")
+
+    }
+}
