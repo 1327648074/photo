2c3005930834183cf6aaefcbcbe368ed9eb0b4da
2017302580221 <43109812+2017302580221@users.noreply.github.com>
2019-07-01 10:57:51 +0800

    7.1.1

--- src/main/java/com/ipa/demo/model/User.java
+++ src/main/java/com/ipa/demo/model/User.java
@@ -10,15 +10,18 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id;//用户id
 
+    //用户邮箱
     private String username;
 
+    //用户密码
     private String password;
 
+    //用户验证码
     private String verCode;
 
-    //0表示未激活，1表示已激活
+    //用户激活状态：0表示未激活，1表示已激活
     private int state;
 
 
